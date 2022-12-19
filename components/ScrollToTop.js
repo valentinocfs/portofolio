ab84302b5149d724de5dd7dca7b5d155199327b6
valentinocfs <valentinostania@gmail.com>
2022-12-19 21:10:41 +0700

    Fix: bug image on meta tag and change some style

--- components/ScrollToTop.js
+++ components/ScrollToTop.js
@@ -21,7 +21,7 @@ export default function ScrollToTop({ className }) {
         <>
             <a
                 href="#home"
-                className={`${displayScrollBox} bg-black-500 border border-white-700 text-white-500 text-sm rounded-md px-2 py-1 transition-all flex items-center z-[90] pb-2  ${addClassName}`}
+                className={`${displayScrollBox} bg-black-500 border border-white-700 text-white-500 text-sm rounded-md px-2 py-2 transition-all flex items-center z-[90] pb-2  ${addClassName}`}
             >
                 <BsChevronDoubleUp size={20} />
             </a>
