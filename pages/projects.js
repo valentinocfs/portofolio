9acd2788e5e49fcdbb683b5bb1ffc5138c0f3874
valentinocfs <valentinostania@gmail.com>
2022-12-23 10:44:36 +0700

    Style: Add animation on projects page

--- pages/projects.js
+++ pages/projects.js
@@ -8,13 +8,21 @@ export default function Projects() {
                         alt="Photo of worker"
                         className="under-construct-image mx-auto"
                         priority="true"
+                        data-aos="fade-up"
+                        data-aos-delay="100"
+                        data-aos-duration="1000"
                     />
-                    <div className="">
+                    <div 
+                        className=""
+                        data-aos="fade-up"
+                        data-aos-delay="200"
+                        data-aos-duration="1000"
+                    >
                         <h1 className="text-xl sm:text-2xl lg:text-4xl pt-4 lg:pt-24 mt-4 lg:mt-0">THIS PAGE IS UNDER CONSTRUCTION!</h1>
                         <p className=" text-white-700 mt-4 md:mt-16 px-12 lg:px-8">Currently working on this website to deliver a better user experience 👌</p>
                     </div>
                 </div>
-                <p className="group pt-5 text-white-700 mt-10 hover:underline transition-all">
+                <p className="group pt-5 text-white-700 mt-10 hover:underline transition-all"  data-aos="fade-up" data-aos-delay="300" data-aos-duration="1000">
                     <a
                         href="/"
                         rel="noopener"
